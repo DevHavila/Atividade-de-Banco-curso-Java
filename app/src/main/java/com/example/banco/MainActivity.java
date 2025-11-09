514b6d5c35e8d758ef135140cae52d925053b913
DevHavila <havilajunior005@gmail.com>
2025-11-09 15:28:00 -0400

    Lógica do Banco e Interface

---
+++ app/src/main/java/com/example/banco/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.banco;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.banco.model.Account;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/banco/model/Account.java
@@ -0,0 +1,57 @@
+package com.example.banco.model;
+
+    public class Account {
+
+        private Integer number;
+        private String holder;
+        private Double balance;
+        private Double withdrawLimit;
+
+        public Account(Integer number, String holder, Double balance, Double withdrawmit) {
+            super();
+            this.number = number;
+            this.holder = holder;
+            this.balance = balance;
+            this.withdrawLimit = withdrawmit;
+        }
+
+        public Integer getNumber() {
+            return number;
+        }
+
+        public void setNumber(Integer number) {
+            this.number = number;
+        }
+
+        public String getHolder() {
+            return holder;
+        }
+
+        public void setHolder(String holder) {
+            this.holder = holder;
+        }
+
+        public String getBalance() {
+            return String.format("%.2f",balance);
+        }
+
+        public Double getWithdrawmit() {
+            return withdrawLimit;
+        }
+
+        public void deposit(Double amount) {
+            balance += amount;
+        }
+
+        public void withdraw(Double amount) throws DomainException {
+            if(amount > withdrawLimit) {
+                throw new DomainException("The amount exceeds withdraw Limit");
+            }
+            if(balance < withdrawLimit) {
+                throw new DomainException("Not enough balance");
+            }
+
+            balance -= amount;
+        }
+
+}
